d14d52c8c7253333698c4169b409614b0bd47d41
Thulunga Basumatary <thulunga.tb@gmail.com>
2026-03-26 12:04:36 +0530

    Fix ViewedBitLockerRecoveryKeysEventTests metadata assertion errors

--- Soti.MobiControl.WindowsModern.Web.Implementation.Tests/ViewedBitLockerRecoveryKeysEventTests.cs
+++ Soti.MobiControl.WindowsModern.Web.Implementation.Tests/ViewedBitLockerRecoveryKeysEventTests.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using NUnit.Framework;
 using Soti.MobiControl.Events;
-using Soti.MobiControl.Events.Attributes;
 using Soti.MobiControl.WindowsModern.Web.Implementation.Events;
 
 namespace Soti.MobiControl.WindowsModern.Web.Implementation.Tests
@@ -22,14 +21,5 @@ namespace Soti.MobiControl.WindowsModern.Web.Implementation.Tests
             Assert.That(testEvent.EventAdditionalParameters, Is.EqualTo(new[] { "admin" }));
         }
 
-        [Test]
-        public void ViewedBitLockerRecoveryKeyEventTests_MetadataMessage()
-        {
-            var metadata = (EventMetadataAttribute)Attribute.GetCustomAttribute(typeof(ViewedBitLockerRecoveryKeysEvent), typeof(EventMetadataAttribute));
-
-            Assert.That(metadata, Is.Not.Null);
-            Assert.That(metadata.Name, Is.EqualTo("ViewedBitLockerRecoveryKeys"));
-            Assert.That(metadata.Description, Is.EqualTo("Administrator has requested to view the BitLocker Recovery Key(s)"));
-        }
     }
 }
